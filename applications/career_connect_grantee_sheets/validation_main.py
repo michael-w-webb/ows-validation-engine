6a4439cc2ad49d93885c5fdf424b83ca1979bca0
Daly <Bobby.Daly@ct.gov>
2026-04-27 11:05:20 -0400

    Adding log description

--- applications/career_connect_grantee_sheets/validation_main.py
+++ applications/career_connect_grantee_sheets/validation_main.py
@@ -63,7 +63,7 @@ cross_rules = [
 
 GRAB_LATEST = False
 LOGGING = True
-LOG_DESCRIPTION = "PY4 Q3 4/24"
+LOG_DESCRIPTION = "PY4 Q3 4/27 TESTING CODE MERGE"
 
 # Daly commented this for testing purposes. 4/22 -> for target_period in ["PY2 Q2", "PY2 Q3", "PY2 Q4", "PY3 Q1", "PY3 Q2", "PY3 Q3", "PY3 Q4","PY4 Q1","PY4 Q2"]:   ### specify period for file selection here. Could be adjusted to loop through all periods if desired. "PY2 Q2", "PY2 Q3", "PY2 Q4", "PY3 Q1", "PY3 Q2", "PY3 Q3", "PY3 Q4", 
 ## "PY2 Q2", "PY2 Q3", "PY2 Q4", "PY3 Q1", "PY3 Q2", "PY3 Q3", "PY3 Q4","PY4 Q1", 
